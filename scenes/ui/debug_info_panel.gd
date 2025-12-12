1df90a07db3c4fc51939fda79cadb91c0c579b10
Cameron Brown <cameron.g.brown@gmail.com>
2025-12-12 11:02:45 -0800

    fixed minor bug where debug_force_info_panel wasn't working. Also added dropped frames counter to DIP

--- scenes/ui/debug_info_panel.gd
+++ scenes/ui/debug_info_panel.gd
@@ -1,11 +1,23 @@
 extends Control
 
-@onready var fps_counter: Label = %FpsCounter
-@onready var frame_time: Label = %FrameTime
+@onready var fps_counter_label: Label = %FpsCounter
+@onready var frame_time_label: Label = %FrameTime
+@onready var dropped_frames_label: Label = %DroppedFrames
+
+var dropped_frames := 0
+var ms_this_frame := 0.0
 
 func _ready() -> void:
 	RH.print("📈 debug_info_panel.gd | _ready()", 1)
+	dropped_frames = 0
 
 func _process(_delta: float) -> void:
-	fps_counter.text = "%.2f FPS" % Performance.get_monitor(Performance.TIME_FPS)
-	frame_time.text = "%.2f ms" % Performance.get_monitor(Performance.TIME_PROCESS)
+	fps_counter_label.text = "%.2f FPS" % Performance.get_monitor(Performance.TIME_FPS)
+	
+	ms_this_frame = Performance.get_monitor(Performance.TIME_PROCESS)
+	frame_time_label.text = "%.2f ms" % ms_this_frame
+
+	if ms_this_frame > 16.0:
+		dropped_frames += 1
+
+	dropped_frames_label.text = "%d dropped" % dropped_frames
