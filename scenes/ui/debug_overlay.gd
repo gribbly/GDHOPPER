374c43b5798ac0b0e6b009a4973965ff804aa1ee
Cameron Brown <cameron.g.brown@gmail.com>
2025-12-12 12:25:44 -0800

    added debug toggle for debug visuals

--- scenes/ui/debug_overlay.gd
+++ scenes/ui/debug_overlay.gd
@@ -1,14 +1,20 @@
 extends Control
 
 @onready var debug_info_toggle: Button = %DebugInfoToggle
+@onready var debug_visuals_toggle: Button = %DebugVisualsToggle
 @onready var back_btn: Button = %Back
 
 func _ready() -> void:
 	RH.print("👾 debug_overlay.gd | _ready()", 1)
 	debug_info_toggle.button_pressed = RH.show_debug_info_panel # make button state match global state
+	debug_visuals_toggle.button_pressed = RH.show_debug_visuals # make button state match global state
 
 	# Button signals
 	debug_info_toggle.toggled.connect(_on_debug_info_toggle_toggled)
+	back_btn.pressed.connect(func(): 
+		App.pause_game()
+	)
+	debug_visuals_toggle.toggled.connect(_on_debug_visuals_toggle_toggled)
 	back_btn.pressed.connect(func(): 
 		App.pause_game()
 	)
@@ -21,3 +27,10 @@ func _on_debug_info_toggle_toggled(toggled_on: bool) -> void:
 	else:
 		RH.show_debug_info_panel = false
 	App.show_debug_info_panel(RH.show_debug_info_panel)
+
+func _on_debug_visuals_toggle_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		RH.show_debug_visuals = true
+	else:
+		RH.show_debug_visuals = false
+		RH.debug_visuals.clear()
