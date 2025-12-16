1016f2b583f2ff831d715c9943102635168dd018
Cameron Brown <cameron.g.brown@gmail.com>
2025-12-16 13:26:11 -0800

    removed stray var

--- scenes/ui/debug_info_panel.gd
+++ scenes/ui/debug_info_panel.gd
@@ -5,7 +5,6 @@ extends Control
 
 # Internal
 var dropped_frames := 0
-var ms_average_frame := 0.0
 var time_info_string: String
 var render_info_string: String
 
@@ -17,9 +16,11 @@ func _process(_delta: float) -> void:
 	if _delta > (1.0 / 60.0):
 		dropped_frames += 1
 
-	time_info_string = "%.2f FPS | %.2fms | %d drop" % [Performance.get_monitor(Performance.TIME_FPS), _delta, dropped_frames]
+	time_info_string = "%.2f FPS | %.2fms | %d drop" % \
+	[Performance.get_monitor(Performance.TIME_FPS), _delta, dropped_frames]
 	time_info.text = time_info_string
 
-	render_info_string = "%d drawcalls" % Performance.get_monitor(Performance.RENDER_TOTAL_DRAW_CALLS_IN_FRAME)
+	render_info_string = "Draw calls: %d" % \
+	Performance.get_monitor(Performance.RENDER_TOTAL_DRAW_CALLS_IN_FRAME)
 	render_info.text = render_info_string
 
